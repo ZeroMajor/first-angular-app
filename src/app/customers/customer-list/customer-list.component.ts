97fa7477dc64a3663a8919e0a10063adac9f84b4
ZeroMajor <zeromajor@outlook.com>
2019-05-02 15:42:25 +0300

    Shared items

--- src/app/customers/customer-list/customer-list.component.ts
+++ src/app/customers/customer-list/customer-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ICustomer } from '../../shared/interfaces';
 
 @Component({
   selector: 'app-customer-list',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./customer-list.component.css']
 })
 export class CustomerListComponent implements OnInit {
-  filteredCustomers: any[] = [];
+  filteredCustomers: ICustomer[] = [];
   customersOrderTotal: number;
   currencyCode: string = 'USD';
 
