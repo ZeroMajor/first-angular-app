c2d25acc319d3d135bda6d461ce15c3c10d6227f
ZeroMajor <zeromajor@outlook.com>
2019-05-02 13:11:27 +0300

    OnInit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'first-angular-app';
+export class AppComponent implements OnInit {
+  title: string;
+
+  ngOnInit() {
+    this.title = 'Hello world on Init';
+  }  
 }
