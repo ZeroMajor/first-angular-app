2d10e38721c2100e85e3bdbecf54d3d462cffc70
ZeroMajor <zeromajor@outlook.com>
2019-05-02 14:26:32 +0300

    customers' list (dummy)

--- src/app/customers/customers.module.ts
+++ src/app/customers/customers.module.ts
@@ -2,9 +2,10 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 import { CustomersComponent } from './customers.component';
+import { CustomerListComponent } from './customer-list/customer-list.component';
 
 @NgModule({
-    declarations: [CustomersComponent],
+    declarations: [CustomersComponent, CustomerListComponent],
     imports: [CommonModule],
     exports: [CustomersComponent]
 })
